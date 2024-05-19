d90a3f01a0bb58459813ccbaf5d87593a8fc6267
CopiaPiratA <aleustrsa2004@gmail.com>
2024-05-19 20:54:33 -0300

    responsive carrusel

--- src/components/carrusels/Carrusel.tsx
+++ src/components/carrusels/Carrusel.tsx
@@ -5,30 +5,36 @@ import styles from './carrusel.module.css';
 interface CarruselProps {
     imgs: string[];
     titles?: string[];
-    onClick?: (value:string)=> void
+    onClick?: (value: string) => void;
 }
 
-export function Carrusel({ imgs, titles,onClick }: CarruselProps) {
+export function Carrusel({ imgs, titles, onClick }: CarruselProps) {
     const [currentIdx, setCurrentIdx] = useState(0);
 
     const moveLeft = () => {
-        setCurrentIdx(prevIdx => Math.max(prevIdx - 1, 0));
+        setCurrentIdx(prevIdx => (prevIdx === 0 ? imgs.length - 1 : prevIdx - 1));
     };
 
     const moveRight = () => {
-        setCurrentIdx(prevIdx => Math.min(prevIdx + 1, imgs.length - 3));
+        setCurrentIdx(prevIdx => (prevIdx === imgs.length - 1 ? 0 : prevIdx + 1));
     };
 
     const renderImages = () => {
-        return imgs.slice(currentIdx, currentIdx + 3).map((img, index) => {
-            const actualIndex = currentIdx + index;
+        const displayedImages = [
+            imgs[(currentIdx + imgs.length - 1) % imgs.length],
+            imgs[currentIdx],
+            imgs[(currentIdx + 1) % imgs.length]
+        ];
+
+        return displayedImages.map((img, index) => {
+            const actualIndex = (currentIdx + index - 1 + imgs.length) % imgs.length;
             const altText = titles && titles[actualIndex] ? `${titles[actualIndex]} - ${img}` : img;
             const isMiddleImg = index === 1;
 
             return (
                 <div 
                     key={actualIndex} 
-                    className={`${styles.carruselImgContainer} ${isMiddleImg ? styles.focusedImg : ''}`}
+                    className={`${styles.carruselImg} ${isMiddleImg ? styles.focusedImg : ''}`}
                     onClick={() => {
                         if (index === 0) moveLeft();
                         else if (index === 1) {
@@ -44,9 +50,26 @@ export function Carrusel({ imgs, titles,onClick }: CarruselProps) {
         });
     };
 
+    const renderDots = () => {
+        return imgs.map((_, index) => {
+            const isActive = index === currentIdx;
+            return (
+                <div 
+                    key={index} 
+                    className={`${styles.dot} ${isActive ? styles.activeDot : ''}`}
+                />
+            );
+        });
+    };
+
     return (
         <div className={styles.carruselContainer}>
-            {renderImages()}
+           <div className={styles.carruselImgContainer}>
+           {renderImages()}
+           </div>
+            <div className={styles.dotsContainer}>
+                {renderDots()}
+            </div>
         </div>
     );
 }
